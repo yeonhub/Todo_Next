a90e89263b15a557ff8c64a129561952f897b03d
yeonhub-desktop <lsy3237@gmail.com>
2024-03-20 15:15:37 +0900

    [REMOVE] 불필요 import 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
+import { title } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import TodosTable from "@/components/todosTable"
 
