2340aa739efb7c473c49180bdda53ffd6558d6a7
yeonhub <lsy3237@gmail.com>
2024-03-22 12:51:04 +0900

    [FEAT] table loading component 추가

---
+++ components/todosLoading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const todosLoading = () => {
+    return (
+        <div>
+          <span>Loading</span>  
+        </div>
+    );
+};
+
+export default todosLoading;
